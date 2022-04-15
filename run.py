e9561e922d17d9fd9ea92755e5e7291d396cf90e
Amr Sharaf <amrsharaf@microsoft.com>
2022-04-15 08:25:21 +0000

    a test case for tokenize and align labels

---
+++ run.py
@@ -0,0 +1,17 @@
+from src.mirror_bert import MirrorBERT
+
+model_name = "cambridgeltl/mirror-roberta-base-sentence-drophead"
+mirror_bert = MirrorBERT()
+mirror_bert.load_model(path=model_name, use_cuda=True)
+
+embeddings = mirror_bert.get_embeddings([
+    'I transform pre-trained language models into universal text encoders.',
+], agg_mode="tokens")
+print (embeddings.shape)
+examples = {'tokens': 'I transform pre-trained language models into universal text encoders .', 
+    'tags': ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'] }
+label_to_id = {'O': 1}
+label_all_tokens = False
+mirror_bert.tokenize_and_align_labels(examples=examples, label_to_id=None, label_all_tokens=label_all_tokens, 
+    b_to_i_label=None)
+print('done')
