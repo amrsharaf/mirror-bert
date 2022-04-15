f22efdb3abd082e8e16b285f3e563d85a660a863
Amr Sharaf <amrsharaf@microsoft.com>
2022-04-15 08:59:35 +0000

    working version

--- run.py
+++ run.py
@@ -8,10 +8,12 @@ embeddings = mirror_bert.get_embeddings([
     'I transform pre-trained language models into universal text encoders.',
 ], agg_mode="tokens")
 print (embeddings.shape)
-examples = {'tokens': ['I transform pre-trained language models into universal text encoders .'], 
+examples = {'tokens': [['I', 'transform', 'pre-trained', 'language', 'models', 'into', 'universal', 'text', 'encoders', '.']], 
     'tags': [['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']] }
 label_to_id = {'O': 1}
 label_all_tokens = False
-mirror_bert.tokenize_and_align_labels(examples=examples, label_to_id=label_to_id, label_all_tokens=label_all_tokens, 
-    b_to_i_label=None)
+tokenized_inputs = mirror_bert.tokenize_and_align_labels(examples=examples, label_to_id=label_to_id, 
+    label_all_tokens=label_all_tokens, b_to_i_label=None)
+print('tokenized_inputs: ')
+print(tokenized_inputs)
 print('done')
